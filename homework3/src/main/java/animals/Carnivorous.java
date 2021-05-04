3b0249b1b41ad597fede1ed2571262d03afbd1bd
vslve <ktiiczcthddm@mail.ru>
2021-05-04 21:26:59 +0300

    homework3

--- homework3/src/main/java/animals/Carnivorous.java
+++ homework3/src/main/java/animals/Carnivorous.java
@@ -4,7 +4,7 @@ import aviary.AviarySize;
 import food.Food;
 import food.Meat;
 
-public class Carnivorous extends Animal {
+public abstract class Carnivorous extends Animal {
     public Carnivorous(AviarySize aviarySize) {
         super(aviarySize);
         this.foodType = Meat.class;
--- homework3/src/main/java/animals/Herbivore.java
+++ homework3/src/main/java/animals/Herbivore.java
@@ -4,7 +4,7 @@ import aviary.AviarySize;
 import food.Food;
 import food.Grass;
 
-public class Herbivore extends Animal {
+public abstract class Herbivore extends Animal {
     public Herbivore(AviarySize aviarySize) {
         super(aviarySize);
         this.foodType = Grass.class;
